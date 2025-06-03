51967fe7d65ce7739939b56f0c894bc5e33a8c6e
带带大富豪 <your.email@example.com>
2025-06-03 23:21:22 +0800

    补充缺少的文件

---
+++ rathena/src/custom/defines_pre.hpp
@@ -0,0 +1,32 @@
+#ifndef CUSTOM_DEFINES_PRE_HPP
+#define CUSTOM_DEFINES_PRE_HPP
+
+// 服务器名称
+#define SERVER_NAME "RO Private Server"
+
+// 服务器版本
+#define SERVER_VERSION "1.0.0"
+
+// 最大在线人数
+#define MAX_CONNECTION 1000
+
+// 是否启用 PVP
+#define PVP_ENABLED 1
+
+// 是否启用 GVG
+#define GVG_ENABLED 1
+
+// 是否启用自动保存
+#define AUTOSAVE_INTERVAL 300
+
+// 是否启用经验值倍率
+#define BASE_EXP_RATE 100
+#define JOB_EXP_RATE 100
+
+// 是否启用掉落倍率
+#define DROP_RATE 100
+
+// 是否启用金钱倍率
+#define MONEY_RATE 100
+
+#endif // CUSTOM_DEFINES_PRE_HPP 
